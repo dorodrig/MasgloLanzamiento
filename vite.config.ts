b6c0e5f5e35bb51a61b6e21b6de987a9594ba712
dorodrig <rodriguezdavid386@gmail.com>
2025-09-09 22:23:36 -0500

    fix: agregando paso a paso para despligue en github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : 'https://dorodrig.github.io/MasgloLanzamiento',
   plugins: [react()],
+  base : 'https://dorodrig.github.io/MasgloLanzamiento',
 })
